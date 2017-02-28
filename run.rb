f91cac0364d90e0410eadad267b648f38fcd89bb
origami ouch <origami ouch>
2017-02-28 22:32:44 +0900

    Main process file is added.

---
+++ run.rb
@@ -0,0 +1,25 @@
+require_relative './src/rmSvgTrns'
+require 'rexml/document'
+require 'logger'
+
+file_path = ARGV[0]
+dest_file_path = ARGV[1]
+
+if file_path.nil? || dest_file_path.nil?
+	p 'parameters are needed: svg_file_path dest_file_path'
+end
+
+remover = SVGTransformRemover.new
+remover.log_level = Logger::INFO
+
+svg_document = REXML::Document.new(File.open(file_path))
+
+begin
+	remover.apply svg_document.root
+rescue => e
+	p 'Exited by error.'
+	p e
+	exit
+end
+remover.write
+#remover.write File.open(dest_file_path)
